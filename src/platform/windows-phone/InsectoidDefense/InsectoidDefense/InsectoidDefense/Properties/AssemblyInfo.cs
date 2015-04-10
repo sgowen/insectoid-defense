570a904f42329986b809e40c2557d1e67e039282
Stephen Gowen <dev.sgowen@gmail.com>
2015-04-10 10:23:54 -0400

    Changing version to 1.0.0.0, renaming TECHNE_GAMES to GGD, and updating copyright

--- src/platform/windows-phone/InsectoidDefense/InsectoidDefense/InsectoidDefense/Properties/AssemblyInfo.cs
+++ src/platform/windows-phone/InsectoidDefense/InsectoidDefense/InsectoidDefense/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("InsectoidDefense")]
-[assembly: AssemblyCopyright("Copyright © Stephen Gowen 2014")]
+[assembly: AssemblyCopyright("Copyright © Stephen Gowen 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
